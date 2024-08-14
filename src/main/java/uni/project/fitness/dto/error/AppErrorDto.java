e7b2c5ee9f529342123a8a3782b4a5e1b0d03d02
jahongir-sobirjonov <sjahongir221@gmail.com>
2024-08-14 12:10:02 +0500

    Error DTOs

---
+++ src/main/java/uni/project/fitness/dto/error/AppErrorDto.java
@@ -0,0 +1,35 @@
+package uni.project.fitness.dto.error;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
+public class AppErrorDto {
+    private final String friendlyMessage;
+    private final Object developerMessage;
+    private final String errorPath;
+    private final Integer errorCode;
+
+    public AppErrorDto(String friendlyMessage, String errorPath, Integer errorCode) {
+        this(friendlyMessage, friendlyMessage, errorPath, errorCode);
+    }
+
+    public AppErrorDto(String friendlyMessage, Object developerMessage, String errorPath, Integer errorCode) {
+        this.friendlyMessage = friendlyMessage;
+        this.developerMessage = developerMessage;
+        this.errorPath = errorPath;
+        this.errorCode = errorCode;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                \n
+                *friendlyMessage : %s*
+                *errorPath : %s*
+                *errorCode : %s*
+                *developerMessage* : `%s`
+                """.formatted(friendlyMessage, errorPath, errorCode, developerMessage);
+    }
+}
