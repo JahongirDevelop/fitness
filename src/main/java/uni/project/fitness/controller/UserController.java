70205a3fdef1f9dabd92ef42ef86a61b0c682571
jahongir-sobirjonov <sjahongir221@gmail.com>
2024-08-19 10:24:56 +0500

    Updated UserEntity and UserController

--- src/main/java/uni/project/fitness/controller/UserController.java
+++ src/main/java/uni/project/fitness/controller/UserController.java
@@ -1,4 +1,28 @@
 package uni.project.fitness.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uni.project.fitness.entity.UserEntity;
+import uni.project.fitness.servise.UserService;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserEntity> getUserById(@PathVariable UUID id) {
+        return userService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable UUID id) {
+        return ResponseEntity.status(200).body(userService.deleteUser(id));
+    }
 }
