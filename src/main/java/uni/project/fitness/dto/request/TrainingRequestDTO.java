6272c0f37265b5566cdaccc15ee2defd8ed04444
jahongir-sobirjonov <sjahongir221@gmail.com>
2024-08-21 14:58:20 +0500

    Added TrainingRequest and TrainingService

---
+++ src/main/java/uni/project/fitness/dto/request/TrainingRequestDTO.java
@@ -0,0 +1,25 @@
+package uni.project.fitness.dto.request;
+
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TrainingRequestDTO {
+    private String title;
+    private UUID teacherId; // Teacher ID
+    private UUID courseId;
+    private String description;
+    private String shortDescription;
+    private String image;
+    private String video;
+    private String importantInfo;
+    private List<String> equipments;
+    private List<String> musclesInvolved;
+    private List<String> results;
+}
