f1ce5802796b25eac0caf9cffc2a4364727ba90b
jahongir-sobirjonov <sjahongir221@gmail.com>
2024-08-21 14:57:10 +0500

    Added TeacherEntity and TeacherResponseDTO

---
+++ src/main/java/uni/project/fitness/dto/response/TeacherResponseDTO.java
@@ -0,0 +1,20 @@
+package uni.project.fitness.dto.response;
+
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TeacherResponseDTO {
+    private UUID id;
+    private String name;
+    private String image;
+    private String workType;
+    private String about;
+    private List<UUID> trainings;
+}
---
+++ src/main/java/uni/project/fitness/entity/Teacher.java
@@ -0,0 +1,28 @@
+package uni.project.fitness.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Entity(name = "teacher")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Teacher extends BaseEntity {
+    private String name;
+    private String image;
+    private String workType;
+    private String about;
+
+    @OneToMany(mappedBy = "teacher", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<Training> trainingList;
+}
+
+
+
