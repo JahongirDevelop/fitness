6cc93b4820cf30eae71b3509909210159f72bb27
jahongir-sobirjonov <sjahongir221@gmail.com>
2024-08-21 15:03:35 +0500

    Added CourseController and CourseRepository

---
+++ src/main/java/uni/project/fitness/controller/CourseController.java
@@ -0,0 +1,44 @@
+package uni.project.fitness.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uni.project.fitness.dto.request.CourseRequestDTO;
+import uni.project.fitness.dto.response.CourseResponseDTO;
+import uni.project.fitness.servise.CourseService;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/courses")
+@RequiredArgsConstructor
+public class CourseController {
+    private final CourseService courseService;
+
+    @PostMapping
+    public ResponseEntity<CourseResponseDTO> createCourse(@RequestBody CourseRequestDTO requestDTO) {
+        return ResponseEntity.ok(courseService.createCourse(requestDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CourseResponseDTO> updateCourse(@PathVariable UUID id, @RequestBody CourseRequestDTO requestDTO) {
+        return ResponseEntity.ok(courseService.updateCourse(id, requestDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCourse(@PathVariable UUID id) {
+        courseService.deleteCourse(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CourseResponseDTO> getCourseById(@PathVariable UUID id) {
+        return ResponseEntity.ok(courseService.getCourseById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CourseResponseDTO>> getAllCourses() {
+        return ResponseEntity.ok(courseService.getAllCourses());
+    }
+}
---
+++ src/main/java/uni/project/fitness/repository/CourseRepository.java
@@ -0,0 +1,12 @@
+package uni.project.fitness.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uni.project.fitness.entity.Category;
+import uni.project.fitness.entity.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+public interface CourseRepository extends JpaRepository<Course, UUID> {
+    List<Course> findAllByCategory(Category category);
+}
