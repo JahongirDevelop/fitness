7e9e8911abf686f4a920fe97d46f97de648b176a
jahongir-sobirjonov <sjahongir221@gmail.com>
2024-08-21 17:05:19 +0500

    Changes

--- src/main/java/uni/project/fitness/controller/TrainingController.java
+++ src/main/java/uni/project/fitness/controller/TrainingController.java
@@ -1,23 +1,10 @@
 package uni.project.fitness.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import uni.project.fitness.dto.request.TrainingRequestDTO;
-import uni.project.fitness.dto.response.TrainingResponseDTO;
-import uni.project.fitness.servise.TrainingService;
-
-import java.util.List;
-import java.util.UUID;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uni.project.fitness.dto.request.TrainingRequestDTO;
 import uni.project.fitness.dto.response.TrainingResponseDTO;
 import uni.project.fitness.servise.TrainingService;
-
 import java.util.List;
 import java.util.UUID;
 
