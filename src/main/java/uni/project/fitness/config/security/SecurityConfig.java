8eafe8fefb89c8359c48feee4305a8329acc6ded
jahongir-sobirjonov <sjahongir221@gmail.com>
2024-08-21 15:08:37 +0500

    Changed JwtService on AA package

--- src/main/java/uni/project/fitness/config/security/SecurityConfig.java
+++ src/main/java/uni/project/fitness/config/security/SecurityConfig.java
@@ -1,101 +1,41 @@
-//package uni.project.fitness.config.security;
-//
-//import jakarta.validation.constraints.NotNull;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationProvider;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-//
-//@Configuration
-//@EnableWebSecurity
-//@RequiredArgsConstructor
-//@EnableMethodSecurity(securedEnabled = true)
-//public class SecurityConfig {
-//
-//    private final JwtService jwtService;
-//    private final AuthenticationService authenticationService;
-//    private final JwtFilter jwtAuthFilter;
-//    private final AuthenticationProvider authenticationProvider;
-//
-//    @Bean
-//    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-//        return http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(authz -> authz
-//                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger/**").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .addFilterBefore(new JwtFilter(jwtService, authenticationService),
-//                        UsernamePasswordAuthenticationFilter.class
-//                )
-//                .sessionManagement(sessionManagement -> {
-//                    sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//                })
-//                .build();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .cors(cors -> corsConfigurer())
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(req ->
-//                        req.requestMatchers(
-//                                        "/api/v1/auth/**",
-//                                        "/v2/api-docs",
-//                                        "/v3/api-docs",
-//                                        "/v3/api-docs/**",
-//                                        "/swagger-resources",
-//                                        "/swagger-resources/**",
-//                                        "/configuration/ui",
-//                                        "/configuration/security",
-//                                        "/swagger-ui/**",
-//                                        "/webjars/**",
-//                                        "/swagger-ui.html"
-//                                )
-//                                .permitAll()
-//                                .anyRequest()
-//                                .authenticated()
-//                )
-//                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
-//                .authenticationProvider(authenticationProvider)
-//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(@NotNull CorsRegistry registry) {
-//                registry
-//                        .addMapping("/**")
-//                        .allowedMethods(CorsConfiguration.ALL)
-//                        .allowedHeaders(CorsConfiguration.ALL)
-//                        .allowedOriginPatterns(CorsConfiguration.ALL)
-//                        .allowCredentials(true);
-//            }
-//        };
-//    }
-//}
+package uni.project.fitness.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfig {
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(new JwtFilter(jwtService, authenticationService),
+                        UsernamePasswordAuthenticationFilter.class
+                )
+                .sessionManagement(sessionManagement -> {
+                    sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
