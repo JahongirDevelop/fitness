c70c4c7d7b7a5638202050ae329c2cbf83a4f7e4
jahongir-sobirjonov <sjahongir221@gmail.com>
2024-08-21 15:04:20 +0500

    Added CategoryRequestDTO and CategoryResponseDTO

---
+++ src/main/java/uni/project/fitness/dto/request/CategoryRequestDTO.java
@@ -0,0 +1,18 @@
+package uni.project.fitness.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryRequestDTO {
+    private String name;
+}
+
