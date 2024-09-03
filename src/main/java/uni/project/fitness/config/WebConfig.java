bac932e32cc91cb67c8941a860a1a4895f81738b
JahongirDevelop <sjahongir221@gmail.com>
2024-09-03 10:53:57 +0500

    CORS Error solution

---
+++ src/main/java/uni/project/fitness/config/WebConfig.java
@@ -0,0 +1,19 @@
+package uni.project.fitness.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        FilterRegistrationBean<ForwardedHeaderFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new ForwardedHeaderFilter());
+        filterRegistrationBean.setOrder(0);
+        return filterRegistrationBean;
+    }
+}
+
