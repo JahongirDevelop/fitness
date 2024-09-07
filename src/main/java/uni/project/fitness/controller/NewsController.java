a76e0a20b8525129aa26fc725d7d2238bd06bfcb
JahongirDevelop <sjahongir221@gmail.com>
2024-09-07 20:21:04 +0500

    updated NewsController

--- src/main/java/uni/project/fitness/controller/NewsController.java
+++ src/main/java/uni/project/fitness/controller/NewsController.java
@@ -3,42 +3,49 @@ package uni.project.fitness.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import uni.project.fitness.dto.request.NewsRequestDTO;
+import uni.project.fitness.dto.response.CategoryDTO;
+import uni.project.fitness.dto.response.NewsResponseDTO;
+import uni.project.fitness.entity.Category;
 import uni.project.fitness.entity.News;
 import uni.project.fitness.servise.NewsService;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/news")
 @RequiredArgsConstructor
-
 public class NewsController {
     private final NewsService newsService;
 
     @GetMapping("/get-all-news")
-    public List<News> getAllNews() {
+    public List<NewsResponseDTO> getAllNews() {
         return newsService.findAll();
     }
 
     @GetMapping("/get-news-by-id/{id}")
-    public ResponseEntity<News> getNewsById(@PathVariable UUID id) {
-        Optional<News> news = newsService.findById(id);
-        return news.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    public ResponseEntity<NewsResponseDTO> getNewsById(@PathVariable UUID id) {
+        return newsService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping("/create-news")
-    public News createNews(@RequestBody News news) {
-        return newsService.save(news);
+    public ResponseEntity<NewsResponseDTO> createNews(@RequestBody NewsRequestDTO newsRequest) {
+        NewsResponseDTO createdNews = newsService.save(newsRequest);
+        return ResponseEntity.ok(createdNews);
     }
 
-    @PutMapping("/update-news//{id}")
-    public ResponseEntity<News> updateNews(@PathVariable UUID id, @RequestBody News updatedNews) {
-        return ResponseEntity.ok(newsService.update(id, updatedNews));
+    @PutMapping("/update-news/{id}")
+    public ResponseEntity<NewsResponseDTO> updateNews(@PathVariable UUID id, @RequestBody NewsRequestDTO newsRequest) {
+        NewsResponseDTO updatedNews = newsService.update(id, newsRequest);
+        return ResponseEntity.ok(updatedNews);
     }
 
-    @DeleteMapping("/delete-news//{id}")
+    @DeleteMapping("/delete-news/{id}")
     public ResponseEntity<Void> deleteNews(@PathVariable UUID id) {
         newsService.deleteById(id);
         return ResponseEntity.noContent().build();
