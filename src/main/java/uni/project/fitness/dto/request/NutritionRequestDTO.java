37e52d2816090e27ede6cfe09e207d3cde1432c7
JahongirDevelop <sjahongir221@gmail.com>
2024-09-09 14:36:54 +0500

    created NutritionDTO(request, response)

---
+++ src/main/java/uni/project/fitness/dto/request/NutritionRequestDTO.java
@@ -0,0 +1,18 @@
+package uni.project.fitness.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NutritionRequestDTO {
+    private String title;
+    private String subTitle;
+    private String image;
+    private List<String> description;
+}
+
---
+++ src/main/java/uni/project/fitness/dto/response/NutritionResponseDTO.java
@@ -0,0 +1,20 @@
+package uni.project.fitness.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NutritionResponseDTO {
+    private UUID id;
+    private String title;
+    private String subTitle;
+    private String image;
+    private List<String> description;
+}
+
