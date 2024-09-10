05b0386b5d2d2e87571d286382561645227d7011
JahongirDevelop <sjahongir221@gmail.com>
2024-09-10 14:58:31 +0500

    updates

---
+++ src/main/java/uni/project/fitness/dto/response/CourseTrailerDTO.java
@@ -0,0 +1,18 @@
+package uni.project.fitness.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CourseTrailerDTO {
+    private UUID id;
+    private String trailerVideo;
+}
+
