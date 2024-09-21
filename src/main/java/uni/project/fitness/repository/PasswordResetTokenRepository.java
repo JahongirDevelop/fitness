4f17e714e7035917058615ca01d1b3b7411a02ae
JahongirDevelop <sjahongir221@gmail.com>
2024-09-21 11:39:37 +0500

    Updated PasswordResetTokenRepository

--- src/main/java/uni/project/fitness/repository/PasswordResetTokenRepository.java
+++ src/main/java/uni/project/fitness/repository/PasswordResetTokenRepository.java
@@ -3,10 +3,13 @@ package uni.project.fitness.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uni.project.fitness.dto.auth.PasswordResetToken;
+
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, UUID> {
+    Optional<PasswordResetToken> findByCodeAndUserId(String code, UUID user_id);
+
     Optional<PasswordResetToken> findByCode(String code);
 }
