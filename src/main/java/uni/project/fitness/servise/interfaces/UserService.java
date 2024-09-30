e10a4d2230b2437ac2958454e23965038b1e5568
JahongirDevelop <sobirjonovjahongir133@gmail.com>
2024-09-30 13:27:32 +0500

    added UserService interface

--- src/main/java/uni/project/fitness/servise/interfaces/UserService.java
+++ src/main/java/uni/project/fitness/servise/interfaces/UserService.java
@@ -1,4 +1,17 @@
 package uni.project.fitness.servise.interfaces;
 
+import uni.project.fitness.dto.response.UserResponse;
+import uni.project.fitness.entity.UserEntity;
+import uni.project.fitness.entity.enums.UserRole;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserService {
+    Optional<UserResponse> findById(UUID id);
+    Optional<UserEntity> findByEmail(String email);
+    String deleteUser(UUID id);
+    UserResponse updateUserRole(UUID userId, UserRole role);
+    List<UserResponse> getAll();
 }
