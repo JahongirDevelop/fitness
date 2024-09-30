f1116712b6bb916e16e68c21d167d994acba8c01
JahongirDevelop <sobirjonovjahongir133@gmail.com>
2024-09-30 13:26:57 +0500

    updated UserServiceImpl

--- src/main/java/uni/project/fitness/servise/implement/UserServiceImpl.java
+++ src/main/java/uni/project/fitness/servise/implement/UserServiceImpl.java
@@ -1,4 +1,4 @@
-package uni.project.fitness.servise;
+package uni.project.fitness.servise.implement;
 
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -8,20 +8,20 @@ import uni.project.fitness.entity.UserEntity;
 import uni.project.fitness.entity.enums.UserRole;
 import uni.project.fitness.exception.DataNotFoundException;
 import uni.project.fitness.repository.UserRepository;
-import uni.project.fitness.subscription.SubscriptionDTO;
+import uni.project.fitness.servise.interfaces.UserService;
 
 import java.util.*;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
-
+    @Override
     public Optional<UserEntity> findByEmail(String email) {
         return userRepository.findByEmail(email);
     }
-
+    @Override
     public Optional<UserResponse> findById(UUID id) {
         Optional<UserEntity> userEntity = userRepository.findById(id);
         UserResponse userResponse = modelMapper.map(userEntity, UserResponse.class);
@@ -29,12 +29,12 @@ public class UserService {
         userResponse.setRole(userEntity.get().getRole());
         return Optional.of(userResponse);
     }
-
+    @Override
     public String deleteUser(UUID id) {
         userRepository.deleteById(id);
         return "Successfully deleted";
     }
-
+    @Override
     public UserResponse updateUserRole(UUID userId, UserRole role) {
         UserEntity user = userRepository.findById(userId)
                 .orElseThrow(() -> new DataNotFoundException("User not found"));
@@ -45,7 +45,7 @@ public class UserService {
         map.setRole(updatedUserEntity.getRole());
         return map;
     }
-
+    @Override
     public List<UserResponse> getAll() {
         List<UserResponse> userResponses = new ArrayList<>();
         for (UserEntity userEntity : userRepository.findAll()) {
