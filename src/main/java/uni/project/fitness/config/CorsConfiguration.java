5008d0a21e0c3f182c4a7c7090b31e94c51a674c
JahongirDevelop <sobirjonovjahongir133@gmail.com>
2024-10-03 15:44:43 +0500

    updated CorsConfiguration

--- src/main/java/uni/project/fitness/config/CorsConfiguration.java
+++ src/main/java/uni/project/fitness/config/CorsConfiguration.java
@@ -6,8 +6,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
 import java.io.IOException;
 
 @Component
