dee768e4d9f3157ca4df34a23581e38b11f345a2
JahongirDevelop <sobirjonovjahongir133@gmail.com>
2024-10-07 15:51:34 +0500

    updated UserProfileController

--- src/main/java/uni/project/fitness/controller/UserProfileController.java
+++ src/main/java/uni/project/fitness/controller/UserProfileController.java
@@ -1,5 +1,4 @@
 package uni.project.fitness.controller;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,25 +9,20 @@ import uni.project.fitness.servise.interfaces.CompletedCourseService;
 import java.security.Principal;
 import java.util.List;
 import java.util.UUID;
-
 @RestController
 @RequestMapping("/profile")
 @RequiredArgsConstructor
 public class UserProfileController {
     private final UserProfileService userProfileService;
     private final CompletedCourseService completedCourseService;
-
     @GetMapping("/me")
     public ResponseEntity<?> getUserProfile(Principal principal) {
         return ResponseEntity.ok(userProfileService.getUserProfile(UUID.fromString(principal.getName())));
     }
-
-
     @PatchMapping("/update-profile")
     public ResponseEntity<?> updateUserProfile(@RequestBody UserProfileRequest updatedUser, Principal principal) {
         return ResponseEntity.ok(userProfileService.updateUserProfile(UUID.fromString(principal.getName()), updatedUser));
     }
-
     @GetMapping("/my-completed-courses")
     public ResponseEntity<List<CompletedCourseDTO>> getCompletedCourses(Principal principal) {
         List<CompletedCourseDTO> completedCourses = completedCourseService.getCompletedCoursesByUserId(UUID.fromString(principal.getName()));
