9327cf1604d6029dc9e604fdcf8f4ee4cceb3b72
JahongirDevelop <sobirjonovjahongir133@gmail.com>
2024-10-10 14:17:54 +0500

    updated TrainingService and TrainingServiceImpl

--- src/main/java/uni/project/fitness/servise/interfaces/TrainingService.java
+++ src/main/java/uni/project/fitness/servise/interfaces/TrainingService.java
@@ -5,12 +5,14 @@ import uni.project.fitness.dto.response.*;
 import java.util.List;
 import java.util.UUID;
 public interface TrainingService {
-    TrainingResponseDTO createTraining(TrainingRequestDTO requestDTO);
-    TrainingResponseDTO updateTraining(UUID id, TrainingRequestDTO requestDTO);
+    TrainingForCourseResponseDTO createTrainingForCourse(TrainingForCourseRequestDTO requestDTO);
+    TrainingForTeacherResponseDTO createTrainingForTeacher(TrainingForTeacherRequestDTO requestDTO);
+    TrainingForCourseResponseDTO updateTrainingForCourse(UUID id, TrainingForCourseRequestDTO requestDTO);
+    TrainingForTeacherResponseDTO updateTrainingForTeacher(UUID id, TrainingForTeacherRequestDTO requestDTO);
     void deleteTraining(UUID id);
     TrainingResponseDTO getTrainingById(UUID id);
     List<TrainingResponseDTO> getAllTrainings();
-    List<TrainingResponseDTO> getTrainingsByCourse(UUID courseId);
-    List<TrainingResponseDTO> getTrainingsByTeacher(UUID teacherId);
+    List<TrainingForCourseResponseDTO> getTrainingsByCourse(UUID courseId);
+    List<TrainingForTeacherResponseDTO> getTrainingsByTeacher(UUID teacherId);
 }
 
