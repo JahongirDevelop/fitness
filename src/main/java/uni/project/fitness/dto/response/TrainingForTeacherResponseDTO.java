338e304b82d600550eb2db330191618fda1baebf
JahongirDevelop <sobirjonovjahongir133@gmail.com>
2024-10-10 14:16:03 +0500

    added new DTO classes for Training entity

---
+++ src/main/java/uni/project/fitness/dto/response/TrainingForTeacherResponseDTO.java
@@ -0,0 +1,25 @@
+package uni.project.fitness.dto.response;
+
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TrainingForTeacherResponseDTO {
+    private UUID id;
+    private String title;
+    private TeacherResponseDTO teacher;
+    private String description;
+    private String shortDescription;
+    private String image;
+    private String video;
+    private String importantInfo;
+    private List<String> equipments;
+    private List<String> musclesInvolved;
+    private List<String> results;
+}
