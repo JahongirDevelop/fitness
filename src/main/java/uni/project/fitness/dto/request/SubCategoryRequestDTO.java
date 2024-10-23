add6df286abdbb2135eea4d455b5b5bd985918e5
JahongirDevelop <sobirjonovjahongir133@gmail.com>
2024-10-23 11:38:28 +0500

    updated SubCategoryRequestDTO

--- src/main/java/uni/project/fitness/dto/request/SubCategoryRequestDTO.java
+++ src/main/java/uni/project/fitness/dto/request/SubCategoryRequestDTO.java
@@ -7,6 +7,6 @@ import java.util.UUID;
 @Data
 public class SubCategoryRequestDTO {
     private String name;
-    private UUID parentCategoryId; // References the top-level category it belongs to.
-    private String image; // Subcategories have images.
+    private UUID parentCategoryId;
+    private String image;
 }
