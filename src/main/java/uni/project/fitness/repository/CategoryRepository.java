2d70843922fac85267a3c2b96c248565320660ab
JahongirDevelop <sobirjonovjahongir133@gmail.com>
2024-10-28 17:57:21 +0500

    updated CategoryRepository

--- src/main/java/uni/project/fitness/repository/CategoryRepository.java
+++ src/main/java/uni/project/fitness/repository/CategoryRepository.java
@@ -3,11 +3,9 @@ package uni.project.fitness.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uni.project.fitness.entity.Category;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
     List<Category> findByParentCategoryIsNull();
