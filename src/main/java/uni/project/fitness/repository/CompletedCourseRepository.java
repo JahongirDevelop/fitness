39efec413cd33bbfb18f47081940b88471100617
JahongirDevelop <sobirjonovjahongir133@gmail.com>
2024-10-29 13:43:00 +0500

    updated CompletedCourseRepository

--- src/main/java/uni/project/fitness/repository/CompletedCourseRepository.java
+++ src/main/java/uni/project/fitness/repository/CompletedCourseRepository.java
@@ -1,12 +1,11 @@
 package uni.project.fitness.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import uni.project.fitness.entity.CompletedCourse;
-import uni.project.fitness.entity.Course;
-
 import java.util.List;
 import java.util.UUID;
-
+@Repository
 public interface CompletedCourseRepository extends JpaRepository<CompletedCourse, UUID> {
     List<CompletedCourse> findByUserId(UUID userId);
     boolean existsByUserIdAndCourseId(UUID userId, UUID courseId);
