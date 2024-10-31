e98a721631cd005a7cf488c730fc0f13bd99b8bc
JahongirDevelop <sobirjonovjahongir133@gmail.com>
2024-10-31 11:32:52 +0500

    update CourseTrailerDTO

--- src/main/java/uni/project/fitness/dto/response/CourseTrailerDTO.java
+++ src/main/java/uni/project/fitness/dto/response/CourseTrailerDTO.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.UUID;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
