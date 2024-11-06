1f88e446cb9e1ae3515d295d28f1d238d6cfd162
JahongirDevelop <sobirjonovjahongir133@gmail.com>
2024-11-06 15:44:04 +0500

    updated NewsServiceImpl

--- src/main/java/uni/project/fitness/servise/implement/NewsServiceImpl.java
+++ src/main/java/uni/project/fitness/servise/implement/NewsServiceImpl.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class NewsServiceImpl implements NewsService {
