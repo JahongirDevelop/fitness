b53b7a31ededb3b7d59189dddd86efa3166d4543
JahongirDevelop <sobirjonovjahongir133@gmail.com>
2024-11-07 12:31:25 +0500

    updated NutritionResponseDTO

--- src/main/java/uni/project/fitness/dto/response/NutritionResponseDTO.java
+++ src/main/java/uni/project/fitness/dto/response/NutritionResponseDTO.java
@@ -3,10 +3,7 @@ package uni.project.fitness.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 import java.util.UUID;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
