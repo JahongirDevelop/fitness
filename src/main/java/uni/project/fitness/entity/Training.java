85e956f5870b9342f04c5c0be784f9c867c8909d
JahongirDevelop <sobirjonovjahongir133@gmail.com>
2024-11-15 10:25:38 +0500

    updated Training entity

--- src/main/java/uni/project/fitness/entity/Training.java
+++ src/main/java/uni/project/fitness/entity/Training.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.*;
 import java.util.List;
-
 @Entity(name = "training")
 @AllArgsConstructor
 @NoArgsConstructor
