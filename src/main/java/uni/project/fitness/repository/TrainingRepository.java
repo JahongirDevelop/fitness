0b31ba2a46e534c2b6a865d9bf4dd1d217711ef5
JahongirDevelop <sobirjonovjahongir133@gmail.com>
2024-11-15 10:24:16 +0500

    updated TrainingRepository

--- src/main/java/uni/project/fitness/repository/TrainingRepository.java
+++ src/main/java/uni/project/fitness/repository/TrainingRepository.java
@@ -4,10 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uni.project.fitness.entity.Course;
 import uni.project.fitness.entity.Teacher;
 import uni.project.fitness.entity.Training;
-
 import java.util.List;
 import java.util.UUID;
-
 public interface TrainingRepository extends JpaRepository<Training, UUID> {
     List<Training> findByCourse(Course course);
 
