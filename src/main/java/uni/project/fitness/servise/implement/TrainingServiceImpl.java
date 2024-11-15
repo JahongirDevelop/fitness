3fcb58e79fc4a6318ed2cda7d0a7a47fed77077a
JahongirDevelop <sobirjonovjahongir133@gmail.com>
2024-11-15 10:23:22 +0500

    updated TrainingServiceImpl

--- src/main/java/uni/project/fitness/servise/implement/TrainingServiceImpl.java
+++ src/main/java/uni/project/fitness/servise/implement/TrainingServiceImpl.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class TrainingServiceImpl implements TrainingService {
