b277e708dad10c385a97f1b8b647b38c4b414d31
JahongirDevelop <sobirjonovjahongir133@gmail.com>
2024-11-20 14:57:14 +0500

    updated SubCategoryResponseDTO

--- src/main/java/uni/project/fitness/dto/response/SubCategoryResponseDTO.java
+++ src/main/java/uni/project/fitness/dto/response/SubCategoryResponseDTO.java
@@ -1,10 +1,8 @@
 package uni.project.fitness.dto.response;
 
 import lombok.Data;
-
 import java.util.List;
 import java.util.UUID;
-
 @Data
 public class SubCategoryResponseDTO {
     private UUID id;
